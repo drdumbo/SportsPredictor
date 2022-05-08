9130117d43a1379fe12851be7c8cc3cc27f923d3
fjones <francis.p.jones@gmail.com>
2022-05-08 02:37:12 -0700

    fix some of the Outcomes object

--- Outcomes.py
+++ Outcomes.py
@@ -9,13 +9,9 @@ class Outcome:
 class OutcomeHockey(Outcome):
     # calculates outcomes for Hockey Events
     def __init__(self, p1: Participant, p2: Participant):
-        # regulation
-        # g1 = Hockey.goals(p1, p2)
-        # g2 = Hockey.goals(p2, p1)
+        # results are fixed at 50/50 win/loss.  But, should do a Poisson Distribution of goals
+        # for & against, based on each team's overall GOALS-FOR and GOALS-AGAINST stats
         # regulation time result
-        self.results = {"win": 0.5, "loss": 0.5, "tie": 0.0}        # results are fixed at 50/50 win/loss.
-                                                                    # should do a Poisson Distribution of goals
-                                                                    # for & against, based on each team's
-                                                                    # overall GOALS-FOR and GOALS-AGAINST stats
         # overtime if necessary
         # shootout if necessary
+        self.results = {"win": 0.5, "loss": 0.5, "tie": 0.0}
